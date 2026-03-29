845f151e5c22edd65ddcdc7bd995272e6e63b16d
lu0x43 <lucas_caetano123@hotmail.com>
2026-03-29 09:14:24 -0300

    install and configure tailwindcss and more libs

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+} satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 1. Importe o Tailwind para o Vite
+import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+  ],
 })
